900625edbe7030d407fa412b47bc674630069a27
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-10-16 14:42:17 +1300

    started working on the search form

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,5 +1,45 @@
-const SearchForm = () => {
-	return <h2>SearchForm</h2>
-}
+import styled from 'styled-components';
 
-export default SearchForm
+const SearchForm = () => {
+	return (
+		<Wrapper>
+			<h1>Photastic images</h1>
+			<form action='' className='searchForm'>
+				<input type='text' className='searchInput' />
+				<button className='btn'>search</button>
+			</form>
+		</Wrapper>
+	);
+};
+
+const Wrapper = styled.section`
+	.title {
+		color: var(--primary-500);
+	}
+	.searchForm {
+		width: var(--view-width);
+		max-width: var(--fixed-width);
+		margin: 0 auto;
+		margin-top: 2rem;
+		display: grid;
+		grid-template-columns: 1fr auto;
+	}
+
+	.searchInput {
+		border-color: var(--grey-300);
+		transition: var(--darkModeTransition);
+		color: var(--textColor);
+		border-radius: 0;
+
+		/* border-right: none; */
+	}
+
+	.searchForm .btn {
+		border-radius: 0;
+	}
+	.searchForm .btn:hover {
+		background: var(--primary-300);
+		color: var(--black);
+	}
+`;
+export default SearchForm;
