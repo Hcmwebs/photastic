a51a9f849cef549f1b19d41198baac073899d545
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-10-16 13:02:24 +1300

    added the Theme functionality

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,11 +1,31 @@
 import { createContext, useContext,useEffect,useState } from 'react'
 
-
 const AppContext = createContext()
+
+const getInitialDarkMode = () =>{
+  const preferDarkMode = window.matchMedia('(prefers-color-scheme:dark)').matches
+  const storedDarkMode =localStorage.getItem('darkTheme') === 'true'
+
+  return storedDarkMode || preferDarkMode
+
+} 
 const AppProvider = ({children}) => {
-  const greetings = 'Jambo World!!'
+const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode())
+const [searchWord,setSearchWord] = useState('cat')
+
+const toggleTheme = () =>{
+  const newDarkTheme = !isDarkTheme
+  setIsDarkTheme(newDarkTheme)
+  localStorage.setItem('darkTheme',newDarkTheme)
+}
+
+useEffect(() =>{
+document.body.classList.toggle('dark-theme', isDarkTheme)
+
+},[isDarkTheme])
+
   return (
-    <AppContext.Provider value = {{greetings}}>{children}</AppContext.Provider>
+    <AppContext.Provider value = {{isDarkTheme,toggleTheme, searchWord,setSearchWord}}>{children}</AppContext.Provider>
   )
 }
 const useGlobalContext = () => useContext(AppContext)
