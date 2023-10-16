b495f2555a52b60aaf2b9f6717a12c6c31d56ac1
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-10-16 16:25:07 +1300

    set up env file and react query

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,5 +1,21 @@
-const Gallery = () => {
-	return <h2>Gallery</h2>
-}
+import { useQuery } from '@tanstack/react-query';
+import { useGlobalContext } from '../context/AppContext';
+import axios from 'axios';
 
-export default Gallery
+const url = `https://api.unsplash.com/search/photos?client_id=${
+	import.meta.env.VITE_API_KEY
+}`;
+const Gallery = () => {
+	const { searchWord } = useGlobalContext();
+	const response = useQuery({
+		queryKey: ['images'],
+		queryFn: async () => {
+			const result = await axios.get(`url&query=${searchWord}`);
+			console.log(result);
+		},
+	});
+
+	return <h2>Gallery</h2>;
+};
+
+export default Gallery;
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,19 @@
-import { StrictMode } from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { AppProvider } from './context/AppContext.jsx'
-import GlobalStyles from './styles/GlobalStyles.jsx'
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { AppProvider } from './context/AppContext.jsx';
+import GlobalStyles from './styles/GlobalStyles.jsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<StrictMode>
 		<AppProvider>
-			<GlobalStyles />
-			<App />
+			<QueryClientProvider client={queryClient}>
+				<GlobalStyles />
+				<App />
+			</QueryClientProvider>
 		</AppProvider>
 	</StrictMode>
-)
+);
